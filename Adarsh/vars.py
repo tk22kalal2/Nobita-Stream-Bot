359896474422d0f1dc1be9b5fa51cb2fa2fe3edb
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2023-11-15 04:13:59 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -35,5 +35,5 @@ class Var(object):
     else:
         URL = "http://{}/".format(FQDN)
     DATABASE_URL = str(getenv('DATABASE_URL', ''))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', ''))
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', None))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "")).split()))
