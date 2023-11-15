dcd93e2690ab801272c86de515fef6d804ae02dd
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2023-11-15 17:49:58 +0530

    Update render_template.py

--- Adarsh/utils/render_template.py
+++ Adarsh/utils/render_template.py
@@ -45,7 +45,7 @@ async def render_page(id, secure_hash):
     
     <button style="font-size: 20px; background-color: light blue; border: 2px solid black; border-radius: 0; padding: 10px; color: orange;" onclick="window.location.href = 'playit://playerv2/video?url={current_url}&amp;title={file_data.file_name}'">Playit player</button>&nbsp; <br>
     
-    <button style="font-size: 20px; background-color: blue; border: 2px solid black; border-radius: 0; padding: 20px; color: white;" onclick="window.location.href = '{current_url}'">Download Now</button>
+    <button style="font-size: 40px; background-color: blue; border: 4px solid black; border-radius: 0; padding: 10px; color: white;" onclick="window.location.href = '{current_url}'">Download Now</button>
     </center>
 </p>
 </p>
