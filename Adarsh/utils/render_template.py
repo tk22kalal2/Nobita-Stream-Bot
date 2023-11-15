ca6ae23fa02483a29802d30a7de6ee535bc30cad
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2023-11-15 18:40:32 +0530

    Update render_template.py

--- Adarsh/utils/render_template.py
+++ Adarsh/utils/render_template.py
@@ -49,7 +49,7 @@ async def render_page(id, secure_hash):
         
         <br><br> <!-- Add vertical gap here -->
         
-        <button style="font-size: 20px; background-color: blue; border: 2px solid black; border-radius: 0 20px 0 20px; padding: 10px; color: white;" onclick="window.location.href = '{current_url}'">    Download Now    </button>
+        <button style="font-size: 20px; background-color: blue; border: 2px solid black; border-radius: 0 20px 0 20px; padding: 10px; color: white;" onclick="window.location.href = '{current_url}'">.    Download Now    .</button>
     </center>
 </p>
 
