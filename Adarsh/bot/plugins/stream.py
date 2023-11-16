d2fa235807506ec0734952bd457a2853110ca3d6
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2023-11-16 07:22:16 +0530

    Update stream.py

--- Adarsh/bot/plugins/stream.py
+++ Adarsh/bot/plugins/stream.py
@@ -19,6 +19,48 @@ MY_PASS = os.environ.get("MY_PASS", None)
 pass_dict = {}
 pass_db = Database(Var.DATABASE_URL, "ag_passwords")
 
+async def get_messages(client, message_ids):
+    messages = []
+    total_messages = 0
+    while total_messages != len(message_ids):
+        temb_ids = message_ids[total_messages : total_messages + 200]
+        try:
+            msgs = await client.get_messages(
+                chat_id=client.db_channel.id, message_ids=temb_ids
+            )
+        except FloodWait as e:
+            await asyncio.sleep(e.x)
+            msgs = await client.get_messages(
+                chat_id=client.db_channel.id, message_ids=temb_ids
+            )
+        except BaseException:
+            pass
+        total_messages += len(temb_ids)
+        messages.extend(msgs)
+    return messages
+
+
+async def get_message_id(client, message):
+    if (
+        message.forward_from_chat
+        and message.forward_from_chat.id == client.db_channel.id
+    ):
+        return message.forward_from_message_id
+    elif message.forward_from_chat or message.forward_sender_name or not message.text:
+        return 0
+    else:
+        pattern = "https://t.me/(?:c/)?(.*)/(\\d+)"
+        matches = re.match(pattern, message.text)
+        if not matches:
+            return 0
+        channel_id = matches.group(1)
+        msg_id = int(matches.group(2))
+        if channel_id.isdigit():
+            if f"-100{channel_id}" == str(client.db_channel.id):
+                return msg_id
+        elif channel_id == client.db_channel.username:
+            return msg_id
+            
 @StreamBot.on_message(filters.private & filters.user(ADMINS) & filters.command('batch'))
 async def batch(client: Client, message: Message):
     while True:
