24fd057603fbd4792deb8ac4ef5eba99db1a95c7
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2023-11-16 08:04:46 +0530

    Update clients.py

--- Adarsh/bot/clients.py
+++ Adarsh/bot/clients.py
@@ -7,6 +7,32 @@ from pyrogram import Client
 from Adarsh.utils.config_parser import TokenParser
 from . import multi_clients, work_loads, StreamBot
 
+class Bot(Client):
+    def __init__(self):
+        self.LOGGER = LOGGER
+        self.db_channel = None  # Initialize db_channel attribute
+
+    async def start(self):
+        try:
+            db_channel = await self.get_chat(CHANNEL_ID)
+            self.db_channel = db_channel
+            test_message = await self.send_message(
+                chat_id=db_channel.id,
+                text="Test Message",
+                disable_notification=True
+            )
+            await test_message.delete()
+            self.LOGGER(__name__).info(
+                f"CHANNEL_ID Database detected!\n┌ Title: {db_channel.title}\n└ Chat ID: {db_channel.id}\n——"
+            )
+        except FloodWait as e:
+            self.LOGGER(__name__).warning(f"FloodWait: {e}")
+        except Exception as e:
+            self.LOGGER(__name__).error(f"Error during Bot initialization: {e}")
+            self.LOGGER(__name__).warning(
+                f"Make sure @{self.username} is an admin in the DB Channel, and double-check the CHANNEL_ID value. Current Value: {CHANNEL_ID}"
+            )
+            return 
 
 async def initialize_clients():
     multi_clients[0] = StreamBot
