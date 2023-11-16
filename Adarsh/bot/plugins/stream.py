a75be8e267068ae9ae082705788d06b7a1187f6c
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2023-11-16 08:01:00 +0530

    Update stream.py

--- Adarsh/bot/plugins/stream.py
+++ Adarsh/bot/plugins/stream.py
@@ -19,6 +19,35 @@ MY_PASS = os.environ.get("MY_PASS", None)
 pass_dict = {}
 pass_db = Database(Var.DATABASE_URL, "ag_passwords")
 
+from pyrogram import Client
+from pyrogram.errors import FloodWait
+
+class Bot(Client):
+    def __init__(self):
+        self.LOGGER = LOGGER
+        self.db_channel = None  # Initialize db_channel attribute
+
+    async def start(self):
+        try:
+            db_channel = await self.get_chat(CHANNEL_ID)
+            self.db_channel = db_channel
+            test_message = await self.send_message(
+                chat_id=db_channel.id,
+                text="Test Message",
+                disable_notification=True
+            )
+            await test_message.delete()
+            self.LOGGER(__name__).info(
+                f"CHANNEL_ID Database detected!\n┌ Title: {db_channel.title}\n└ Chat ID: {db_channel.id}\n——"
+            )
+        except FloodWait as e:
+            self.LOGGER(__name__).warning(f"FloodWait: {e}")
+        except Exception as e:
+            self.LOGGER(__name__).error(f"Error during Bot initialization: {e}")
+            self.LOGGER(__name__).warning(
+                f"Make sure @{self.username} is an admin in the DB Channel, and double-check the CHANNEL_ID value. Current Value: {CHANNEL_ID}"
+            )
+
 async def get_messages(client, message_ids):
     messages = []
     total_messages = 0
