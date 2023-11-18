36c295de23e5dce2cf522c3663af420af9c1e4bd
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2023-11-19 02:44:48 +0530

    Update stream.py

--- Adarsh/bot/plugins/stream.py
+++ Adarsh/bot/plugins/stream.py
@@ -161,8 +161,8 @@ async def batch(client: Client, message: Message):
 
 @StreamBot.on_message((filters.private) & (filters.document | filters.video | filters.audio | filters.photo) , group=4)    
 async def private_receive_handler(c: Client, m: Message):
-    if bool(CUSTOM_CAPTION) & bool(m.video):
-        caption = CUSTOM_CAPTION.format(previouscaption="" if not m.caption else m.caption.html, filename=m.video.file_name)
+    if bool(CUSTOM_CAPTION) & and bool(m.document) & and bool(m.video):
+        caption = CUSTOM_CAPTION.format(previouscaption="" if not m.caption else m.caption.html, video_filename=m.video.file_name, document_filename=m.document.file_name)
     else:
         caption = "" if not m.caption else m.caption.html
     
