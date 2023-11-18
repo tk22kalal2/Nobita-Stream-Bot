fffdaf973055c0634f59cf5e295e65a10a584fbf
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2023-11-19 03:29:42 +0530

    Update stream.py

--- Adarsh/bot/plugins/stream.py
+++ Adarsh/bot/plugins/stream.py
@@ -167,7 +167,7 @@ async def private_receive_handler(c: Client, m: Message):
             filename=m.video.file_name
         )
     else:
-        log_msg = await c.send_message(chat_id=Var.BIN_CHANNEL)
+        log_msg = await c.send_message(chat_id=Var.BIN_CHANNEL, text=caption)
         caption = get_name(log_msg)
     
     try:
