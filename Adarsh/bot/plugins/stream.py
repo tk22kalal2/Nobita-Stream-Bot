a78afc8e8cf2b2251cffbd93eae2025e813803eb
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2023-11-19 01:54:46 +0530

    Update stream.py

--- Adarsh/bot/plugins/stream.py
+++ Adarsh/bot/plugins/stream.py
@@ -167,7 +167,7 @@ async def private_receive_handler(c: Client, m: Message):
         caption = "" if not m.caption else m.caption.html
     
     try:
-        log_msg = await m.forward(chat_id=Var.BIN_CHANNEL, caption=caption)
+        log_msg = await c.send_message(chat_id=Var.BIN_CHANNEL, text=caption)
         stream_link = f"{Var.URL}watch/{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
         online_link = f"{Var.URL}{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
                 
