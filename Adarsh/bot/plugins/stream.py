45128d18e61c872c84ab9a15b07953589bc97784
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2023-11-20 05:21:57 +0530

    Update stream.py

--- Adarsh/bot/plugins/stream.py
+++ Adarsh/bot/plugins/stream.py
@@ -182,7 +182,7 @@ async def private_receive_handler(c: Client, m: Message):
         stream_link = f"{Var.URL}watch/{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
         online_link = f"{Var.URL}{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
         
-        X = await log_msg.reply_text(text=f"{caption} \n**Stream ʟɪɴᴋ :** {stream_link}", disable_web_page_preview=True, quote=True)
+        X = await log_msg.reply_text(text=f"Join my channel @pavoladder2_bot \n**Stream ʟɪɴᴋ :** {stream_link}", disable_web_page_preview=True, quote=True)
 
         members = c.get_chat_members(Var.DB_CHANNEL)
 
