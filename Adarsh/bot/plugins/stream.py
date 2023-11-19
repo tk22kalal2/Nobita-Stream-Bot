64df29f21ff6f1c69de7c733eed14114b7b9364a
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2023-11-20 01:33:46 +0530

    Update stream.py

--- Adarsh/bot/plugins/stream.py
+++ Adarsh/bot/plugins/stream.py
@@ -171,9 +171,9 @@ async def private_receive_handler(c: Client, m: Message):
                 
         X = await log_msg.reply_text(text=f"{caption} \n**Stream ʟɪɴᴋ :** {stream_link}", disable_web_page_preview=True, quote=True)
         try:
-            await X.forward(chat_id=Var.CB_CHANNEL)
+            await X.forward(chat_id=Var.DB_CHANNEL)
         except Exception as e:
-            print(f"Error forwarding message to CB_CHANNEL: {e}")                 
+            print(f"Error forwarding message to DB_CHANNEL: {e}")                 
         await m.reply_text(
             text=f"{get_name(log_msg)} \n**Stream ʟɪɴᴋ :** {stream_link}", disable_web_page_preview=True, quote=True
         )    
