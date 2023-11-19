2b1675dd4b3f5395ff4f5e82fac1a0f47b133888
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2023-11-19 08:44:13 +0530

    Update and rename c_client.py to custom_client.py

--- Adarsh/bot/custom_client.py
+++ Adarsh/bot/custom_client.py
@@ -1,8 +1,8 @@
-# c_client.py
+# custom_client.py
 
 from pyrogram import Client
 
-class CClient(Client):
+class CustomClient(Client):
     def __init__(self, db_channel, *args, **kwargs):
         self.db_channel = db_channel
         super().__init__(*args, **kwargs)
