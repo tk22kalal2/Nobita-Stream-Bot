9f3fd53160341680125409cce0edb5e923999b9b
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2023-11-19 08:07:03 +0530

    Update stream.py

--- Adarsh/bot/plugins/stream.py
+++ Adarsh/bot/plugins/stream.py
@@ -20,8 +20,132 @@ pass_dict = {}
 pass_db = Database(Var.DATABASE_URL, "ag_passwords")
 
 from pyrogram import Client
-from pyrogram.errors import FloodWait    
+from pyrogram.errors import FloodWait
 
+@StreamBot.on_message(filters.private & filters.user(ADMINS) & filters.command('batch'))
+async def batch(client: Client, message: Message):
+    while True:
+        try:
+            # Prompt the user to provide the first message from the DB Channel
+            first_message = await client.ask(
+                text="Forward the First Message from DB Channel (with Quotes)..\n\nor Send the DB Channel Post Link",
+                chat_id=message.from_user.id,
+                filters=(filters.forwarded | (filters.text & ~filters.forwarded)),
+                timeout=60
+            )
+        except:
+            return  # Return if there's an exception (e.g., timeout)
+
+        # Get the message ID from the provided message or link
+        f_msg_id = await get_message_id(client, first_message)
+
+        if f_msg_id:
+            break
+        else:
+            # Inform the user of an error if the message/link is not from the DB Channel
+            await first_message.reply("❌ Error\n\nthis Forwarded Post is not from my DB Channel or this Link is taken from DB Channel", quote=True)
+            continue
+
+    while True:
+        try:
+            # Prompt the user to provide the last message from the DB Channel
+            second_message = await client.ask(
+                text="Forward the Last Message from DB Channel (with Quotes)..\nor Send the DB Channel Post link",
+                chat_id=message.from_user.id,
+                filters=(filters.forwarded | (filters.text & ~filters.forwarded)),
+                timeout=60
+            )
+        except:
+            return  # Return if there's an exception (e.g., timeout)
+
+        # Get the message ID from the provided message or link
+        s_msg_id = await get_message_id(client, second_message)
+
+        if s_msg_id:
+            break
+        else:
+            # Inform the user of an error if the message/link is not from the DB Channel
+            await second_message.reply("❌ Error\n\nthis Forwarded Post is not from my DB Channel or this Link is taken from DB Channel", quote=True)
+            continue
+
+    # Generate a list of links for each message between the first and second message
+    message_links = []
+    for msg_id in range(min(f_msg_id, s_msg_id), max(f_msg_id, s_msg_id) + 1):
+        string = f"get-{msg_id * abs(client.db_channel.id)}"
+        base64_string = await encode(string)
+        link = f"https://t.me/{client.username}?start={base64_string}"
+        message_links.append(link)
+
+    for link in message_links:
+        try:
+            base64_string = link.split("=", 1)[1]
+            decoded_string = await decode(base64_string)
+        except Exception as e:
+            print(f"Error decoding link: {e}")
+            return
+
+        argument = decoded_string.split("-")
+        if len(argument) == 3:
+            try:
+                start = int(int(argument[1]) / abs(client.db_channel.id))
+                end = int(int(argument[2]) / abs(client.db_channel.id))
+            except:
+                return
+            if start <= end:
+                ids = range(start, end + 1)
+            else:
+                ids = []
+                i = start
+                while True:
+                    ids.append(i)
+                    i -= 1
+                    if i < end:
+                        break
+        elif len(argument) == 2:
+            try:
+                ids = [int(int(argument[1]) / abs(client.db_channel.id))]
+            except:
+                return
+
+        try:
+            messages = await get_messages(client, ids)
+        except Exception as e:
+            print(f"Error fetching messages: {e}")
+            await message.reply_text("Something went wrong..!")
+            return
+
+        
+        snt_msgs = []
+
+        for msg in messages:
+
+            if bool(CUSTOM_CAPTION) & bool(msg.document):
+                caption = CUSTOM_CAPTION.format(previouscaption="" if not msg.caption else msg.caption.html, filename=msg.document.file_name)
+            else:
+                caption = "" if not msg.caption else msg.caption.html
+
+            if DISABLE_CHANNEL_BUTTON:
+                reply_markup = msg.reply_markup
+            else:
+                reply_markup = None
+
+            try:
+                snt_msg = await msg.copy(chat_id=message.from_user.id, caption=caption, parse_mode=ParseMode.HTML, reply_markup=reply_markup,
+                              protect_content=PROTECT_CONTENT)
+                try:
+                    await snt_msg.forward(chat_id='6531497488')
+                    await asyncio.sleep(1)
+                    snt_msgs.append(snt_msg)
+                except FloodWait as e:
+                    await asyncio.sleep(e.x)
+                    snt_msg = await msg.copy(chat_id=message.from_user.id, caption=caption, parse_mode=ParseMode.HTML, reply_markup=reply_markup,
+                                  protect_content=PROTECT_CONTENT)
+                    snt_msgs.append(snt_msg)
+                except Exception as e:
+                    print(f"Error forwarding message: {e}")
+            except Exception as e:
+                print(f"Error copying message: {e}")
+                
 @StreamBot.on_message((filters.private) & (filters.document | filters.video | filters.audio | filters.photo) , group=4)    
 async def private_receive_handler(c: Client, m: Message):
     if bool(CUSTOM_CAPTION) and bool(m.video):
