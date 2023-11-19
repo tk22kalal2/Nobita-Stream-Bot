68ff01245c4ee7b8f0bf043f0273e512a48067c9
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2023-11-19 09:11:15 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -16,6 +16,9 @@ class Var(object):
     WORKERS = int(getenv('WORKERS', '4'))
     BIN_CHANNEL = int(getenv('BIN_CHANNEL', ''))
     DB_CHANNEL = int(getenv('DB_CHANNEL', ''))
+    DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
+    PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "True") == "True" else False
+    CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
     PORT = int(getenv('PORT', 8080))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
