aef6e8dbdf08f211a062dc744542148b5761375b
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2024-01-25 11:44:18 +0530

    Update extra.py

--- Adarsh/bot/plugins/extra.py
+++ Adarsh/bot/plugins/extra.py
@@ -1,1 +1,8 @@
-
+@StreamBot.on_message(filters.regex("DC"))
+async def start(bot, update):
+    text = START_TEXT.format(update.from_user.dc_id)
+    await update.reply_text(
+        text=text,
+        disable_web_page_preview=True,
+        quote=True
+    )
