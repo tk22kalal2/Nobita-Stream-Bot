ca452b7fcf04d7039e6dc26cd6a1e2e6a9dd05fc
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2024-01-29 22:32:28 +0530

    Update stream.py

--- Adarsh/bot/plugins/stream.py
+++ Adarsh/bot/plugins/stream.py
@@ -142,7 +142,8 @@ async def batch(client: Client, message: Message):
                 online_link = f"{Var.URL}{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
                 reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=stream_link)]])
                 await log_msg.edit_reply_markup(reply_markup)
-                X = await msg.reply_text(text=f"{caption} \n**Stream ʟɪɴᴋ :** {stream_link}", disable_web_page_preview=True, quote=True)                                                         
+                f_caption = f"<b>⏯: {caption}</b>"
+                X = await m.reply_text(text=f"<a href='{stream_link}'>{f_caption}</a>", disable_web_page_preview=True, quote=True)                                                         
             except FloodWait as e:
                 print(f"Sleeping for {str(e.x)}s")
                 await asyncio.sleep(e.x)
