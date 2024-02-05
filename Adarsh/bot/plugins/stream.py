cdd1b8019a49b0a219885b28a77d43e6a6b4322f
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2024-02-05 11:04:00 +0530

    Update stream.py

--- Adarsh/bot/plugins/stream.py
+++ Adarsh/bot/plugins/stream.py
@@ -166,8 +166,8 @@ async def private_receive_handler(c: Client, m: Message):
         online_link = f"{Var.URL}{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
         reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("STREAM ⏯️", url=stream_link)]])
         await log_msg.edit_reply_markup(reply_markup)
-        f_caption = f"<b>⏯: {caption}</b>"
-        X = await m.reply_text(text=f"<a href='{stream_link}'>{f_caption}</a>", disable_web_page_preview=True, quote=True)                                         
+        f_caption = f"<b>{caption}</b>"
+        X = await m.reply_text(text=f"<tr><td><a  href="{stream_link}"  target="_blank">{f_caption}</a></td></tr>", disable_web_page_preview=True, quote=True)                                         
     except FloodWait as e:
         print(f"Sleeping for {str(e.x)}s")
         await asyncio.sleep(e.x)
