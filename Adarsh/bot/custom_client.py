8ff65d4efc28f4f071cdc0d5bd22a97035ab5d54
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2024-02-10 22:15:12 +0530

    Update custom_client.py

--- Adarsh/bot/custom_client.py
+++ Adarsh/bot/custom_client.py
@@ -17,12 +17,16 @@ class CustomClient(Client):
             chat_members = await client.get_chat_members(channel_info.id)
             chat_members_list = [member async for member in chat_members]
             
+            # List to hold all message sending tasks
+            send_message_tasks = []
             for member in chat_members_list:
                 if member.user:
-                    try:
-                        # Send the message to each member individually
-                        await client.send_message(chat_id=member.user.id, text=message.text, caption=caption, parse_mode=ParseMode.HTML, reply_markup=reply_markup)
-                    except Exception as e:
-                        print(f"Error sending message to user {member.user.id}: {e}")
+                    # Start a task to send message to each member
+                    task = asyncio.create_task(client.send_message(chat_id=member.user.id, text=message.text, caption=caption, parse_mode=ParseMode.HTML, reply_markup=reply_markup))
+                    send_message_tasks.append(task)
+
+            # Wait for all message sending tasks to complete
+            await asyncio.gather(*send_message_tasks)
         except Exception as e:
             print(f"Error sending message to channel members: {e}")
+
