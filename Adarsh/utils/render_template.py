544b26b20e10d17bc8ca65065a2b560806dc2016
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2024-03-02 00:24:04 +0530

    Update render_template.py

--- Adarsh/utils/render_template.py
+++ Adarsh/utils/render_template.py
@@ -41,13 +41,13 @@ async def render_page(id, secure_hash):
     <center><h5>Click on 👇 button to watch/download in your favorite player</h5></center>
     <center>
     <button style="display: inline-block; border-radius: 5px; border: none; cursor: pointer; padding: 2px auto; text-align: center; text-decoration: none; background-color: #c0e0ff; overflow: hidden;" onclick="window.location.href = 'intent:{current_url}#Intent;package=com.mxtech.videoplayer.ad;S.title={file_data.file_name};end'">
-    <img src="https://i.postimg.cc/L4xq42xt/mxlogo-1-removebg-preview.png" alt="MX PLAYER Logo" style="max-width: 70px; height: 40px; display: block; margin: 0 auto;">
+    <img src="https://i.postimg.cc/L4xq42xt/mxlogo-1-removebg-preview.png" alt="MX PLAYER Logo" style="max-width: 130px; height: 40px; display: block; margin: 0 auto;">
     </button>
 
 <!-- Add a line space here -->
 
 <button style="display: inline-block; border-radius: 5px; border: none; cursor: pointer; padding: 2px auto; text-align: center; text-decoration: none; background-color: #c0e0ff; overflow: hidden;" onclick="window.location.href = 'vlc://{current_url}'">
-    <img src="https://i.postimg.cc/tCs9xqp5/1000029028-removebg-preview.png" alt="MX PLAYER Logo" style="max-width: 70px; height: 40px; display: block; margin: 0 auto;">
+    <img src="https://i.postimg.cc/tCs9xqp5/1000029028-removebg-preview.png" alt="MX PLAYER Logo" style="max-width: 130px; height: 40px; display: block; margin: 0 auto;">
 </button>
 
 <!-- Add another line space here -->
